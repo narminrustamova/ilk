83b36ff981bfc15f3ba7a8842e4abe8a9aa714f7
narminrustamova <37043699+narminrustamova@users.noreply.github.com>
2018-03-04 20:01:00 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 
+
+
+
+            //Console.WriteLine("parolunuzu daxil edin");
+            //var password = Console.ReadLine();
+            //if (password == "admin")
+            //{
+            //    Console.WriteLine("salam sahib");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("giris qadagandir");
+            //}
+
+
+
+
+
+
+            //Console.WriteLine("aldiginiz qiymeti daxil edin");
+            //var qiymet = Convert.ToInt32(Console.ReadLine());
+
+            //if (qiymet < 50)
+            //{
+            //    Console.WriteLine("olmadi");
+
+            //}
+            //else if (qiymet < 70)
+            //{
+            //    Console.WriteLine("meqbul");
+
+            //}
+            //else if (qiymet < 90)
+            //{
+            //    Console.WriteLine("yaxsidir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("kisi salam de");
+            //}
+
+
+
+
+            //qiymet yazsin adlandirsin qiymeti
+
+            //if(qiymet<0 || qiymet > 100)
+            //{
+            //    Console.WriteLine("guz deyil");
+
+            //}
+            //else
+            //{
+            //    if (qiymet < 50)
+            //    {
+            //        Console.WriteLine("olmadi");
+            //    }
+            //    else
+            //    {
+            //        if (qiymet < 70)
+            //        {
+            //            Console.WriteLine("meqbuldur");
+            //        }
+            //        else
+            //        {
+            //            if (qiymet < 90)
+            //            {
+            //                Console.WriteLine("yaxsidir");
+            //            }
+            //            else
+            //            {
+
+            //                    Console.WriteLine("ela");
+
+            //            }
+            //        }
+            //    }
+            //}
+
+
+
+
+
+            //
+            //int elaveet05()
+            //    {
+            //        Console.WriteLine("ilk vezifeni gir");
+            //        var eded = Convert.ToInt32(Console.ReadLine());
+            //        eded = eded + 5;
+            //        return eded;
+
+            //    }
+
+            //    int elaveet08()
+            //    {
+            //        Console.WriteLine("ikinci vezife");
+            //        var eded = Convert.ToInt32(Console.ReadLine());
+            //        eded = eded + 8;
+            //        return eded;
+
+            //    }
+
+            //    int cix4()
+            //    {
+            //        Console.WriteLine("ucuncu vezife");
+            //        var eded = Convert.ToInt32(Console.ReadLine());
+            //        eded = eded - 4;                
+            //        return eded;
+
+            //    }
+            //    void Result()
+            //    {
+            //        Console.WriteLine(cix4() + elaveet05() + elaveet08());
+            //    }
+            //    Result();
+
+
+
+
+
+
+            // // 1den 50 kimi ekrana yazdir
+
+            //var a = 0;
+            //void yazdir()
+            // {
+            //     Console.WriteLine(a);//ekrna yazdir
+            //     a++;//bir bir artir
+            //     if (a < 50)
+            //     {
+            //         yazdir();
+            //     }
+            // }
+            // yazdir();//bu yazilmasa baslangic olmur
+
+
+
+
+
+            //// // 1den 50 kimi ekrana yazdir while ile
+
+            //var a = 0;
+            //while (a < 100)
+            //{
+            //    Console.WriteLine(a++);
+            //}
+
+
+
+
+            // // 1den 50 kimi ekrana yazdir for ile
+        
+            //for(int i=0; i < 50; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+        }
+    }
+}
